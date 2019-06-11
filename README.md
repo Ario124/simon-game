c575c23c9e5159ae5ebef2b8383ce620be292354
Patrik Solis <patrick__solis@hotmail.com>
2019-06-11 11:06:38 +0000

    Updated main.js with the game functions

--- README.md
+++ README.md
@@ -15,15 +15,82 @@ If the user clicks on the correct button, the game will go on to the next level 
 The game will let the user try to get the right order of flashes endlessly, unless the "STRICT" button has been activated. If activated and if the user then fails to match the flashes, it
 will make the game reset and get back to level 1.
 
-## Features
+### User Stories
 
-## Planning
+#### User story 1
+
+* As a user I can click "ON/OFF" to turn the game ON or to turn the game OFF.
+* As a user I am presented with a nice and clean game design.
+* As a user I am presented with a display that shows if the game is ON.
+* As a user I am allowed to jump right into the game, I can also select strict mode.
+* I advance to the next level by clicking to match the game colors/blinks.
+* Once I advance to a new level, I am presented with the same series as previously but with one additional step to match.
+* [Normal Mode] If I as a user select the colors in the wrong order, I am then notified that the match was "wrong" and get to try again, continuing on the same level.
+* [Strict Mode] As a user who fails to match the colors in the right order, I will be brought back to level 1.
+* I can win the game by getting through all 20 levels.
+* As a user I am notified when victory has been granted.
+
+
+## Features
+#### Game Features
+
+These are the game features that users will interact with.
+
+* User can use a checkbox to either turn the game ON or OFF.
+* User can use a checkbox to activate Strict mode.
+* User can start playing by clicking START.
+* Each button has its own sound that activates once clicked.
+* User can keep track of the game level by looking at the display box.
+* Each time the user succeeds with the matching of colors will make the game add one additional color to click.
+* User will get a message letting you know if the attempt to match the colors has failed.
+* When victory is granted, users will get a "WIN" message.
+
+### Features left to implement
+* Navbar
+* Contact page, possibly with emailjs
+* Info with play instructions
+
 
 ## Logics of the game
+#### Logic through Javascript
+
+The game is designed to be simple and and easy to understand.
+
+* Game will wait until Start has been clicked then procceed to control if the game is ON or if Victory has been achieved.
+* If the power is ON and or if Victory has been achieved then the game will run "playGame" Function.
+* The game will then generate 20 random numbers from 1-4 to go through.
+* Checkup is active during player turn to see if the player matches accurately with the game round.
+* If player matches in the wrong order then a message appears to indicate that wrong choice has been made.
+* The game will check if player is at level 20, if true gameVictory will activate, blinking all colors.
+* Once victory, the game will wait some second, then procceed to reset the colors and start the playGame function again.
+* Strict Mode will only let the player fail to match the colors once, after that the level will be reset to 1.
+
 
 ## Technologies used
 
+#### This website uses these technologies
+
+The site design is for the main part HTML & CSS, logic applied through Javascript.
+I've also used Bootstrap and Google Fonts.
+
+* HTML
+* CSS
+* Javascript
+* Bootstrap
+* Google Fonts
+
 ## Testing
+
+#### Testing game functions
+
+1. Click "ON/OFF" to start the game and see if the game is responsive.
+2. Click on "STRICT" to activate strict mode.
+3. Verify that you can't start clicking the colors after "ON" is displayed. This should happen once "START" is activated.
+4. Verify that you can click on the colors once "START" has been clicked.
+3. Display shows the game level once the "START" button is activated.
+4. Verify that the game has its first round.
+5. Once the game has started, sound should match the blinking lights from the selected colors.
+6. Verify so the game only start by playing off 1 blink and not more. This should increase by 1 every level.
 
 ## Deployment
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -32,9 +32,7 @@ const startingButton = document.getElementById("start");
 
 
 
-function playGame(){
-    victory = false;
-}
+
 
 powerOnButton.addEventListener("click", function(){
     if (powerOnButton.checked == true){
@@ -43,8 +41,10 @@ powerOnButton.addEventListener("click", function(){
     } else {
         powerOn = false;
         roundSum.innerHTML = "";
+        resetColor();
+        clearInterval(playTime);
     }
-})
+});
 
 strictModeButton.addEventListener("click", function(){
     if (strictModeButton.checked == true) {
@@ -52,10 +52,207 @@ strictModeButton.addEventListener("click", function(){
     } else {
         strictMode = false;
     }
-})
+});
 
 startingButton.addEventListener("click", function(){
     if (powerOn || victory) {
         playGame();
     }
 });
+
+
+function playGame(){
+    victory = false;
+    assortment = [];
+    playerAssortment =[];
+    blink = 0;
+    playTime = 0;
+    turn = 1;
+    roundSum.innerHTML = "1";
+    doingGood = true;
+    
+    for (var i = 0; i < 20; i++)
+     {
+         assortment.push(Math.floor(Math.random() * 4) +1);
+     }
+     gameTurn = true;
+     playTime = setInterval(gamesTurn, 800);
+}
+
+function gamesTurn(){
+    powerOn = false;
+    if (blink == turn){
+        clearInterval(playTime);
+        
+        resetColor();
+        powerOn = true;
+        gameTurn = false;
+    }
+    
+    if (gameTurn) {
+        resetColor();
+        setTimeout(() => {
+            if (assortment[blink] == 1) first();
+            if (assortment[blink] == 2) second();
+            if (assortment[blink] == 3) third();
+            if (assortment[blink] == 4) fourth();
+            blink++;
+        }, 300);
+    }
+    
+}
+
+function first(){
+    if (sound) {
+        let audio = document.getElementById("simonsound1");
+        audio.play();
+    }
+    sound = true;
+    uLeft.style.backgroundColor = "lightgreen";
+}
+
+function second(){
+    if (sound) {
+        let audio = document.getElementById("simonsound2");
+        audio.play();
+    }
+    sound = true;
+    uRight.style.backgroundColor = "salmon";
+}
+
+function third(){
+    if (sound) {
+        let audio = document.getElementById("simonsound3");
+        audio.play();
+    }
+    sound = true;
+    bLeft.style.backgroundColor = "yellow";
+}
+
+function fourth(){
+    if (sound) {
+        let audio = document.getElementById("simonsound4");
+        audio.play();
+    }
+    sound = true;
+    bRight.style.backgroundColor = "lightskyblue";
+}
+
+
+function resetColor() {
+    uLeft.style.backgroundColor = "darkgreen";
+    uRight.style.backgroundColor = "darkred";
+    bLeft.style.backgroundColor = "olive";
+    bRight.style.backgroundColor = "darkblue";
+}
+
+function blinkColor() {
+    uLeft.style.backgroundColor = "lightgreen";
+    uRight.style.backgroundColor = "salmon";
+    bLeft.style.backgroundColor = "yellow";
+    bRight.style.backgroundColor = "lightskyblue";
+}
+
+
+uLeft.addEventListener("click", function() {
+    if (powerOn && turn) {
+        playerAssortment.push(1);
+        
+        checkUp();
+        first();
+        if(!victory) {
+            setTimeout(() => {
+                resetColor();
+            }, 300);
+        }
+    }
+});
+
+uRight.addEventListener("click", function() {
+    if (powerOn && turn) {
+        playerAssortment.push(2);
+        checkUp();
+        second();
+        if(!victory) {
+            setTimeout(() => {
+                resetColor();
+            }, 300);
+        }
+    }
+});
+
+bLeft.addEventListener("click", function() {
+    if (powerOn && turn) {
+        playerAssortment.push(3);
+        checkUp();
+        third();
+        if(!victory) {
+            setTimeout(() => {
+                resetColor();
+            }, 300);
+        }
+    }
+});
+
+bRight.addEventListener("click", function() {
+    if (powerOn && turn) {
+        playerAssortment.push(4);
+        checkUp();
+        fourth();
+        if(!victory) {
+            setTimeout(() => {
+                resetColor();
+            }, 300);
+        }
+    }
+});
+
+
+function checkUp(){
+    if (playerAssortment[playerAssortment.length - 1] !== assortment[playerAssortment.length -1])
+    doingGood = false;
+    
+    if (playerAssortment.length == 3 && doingGood) {
+        gameVictory();
+    }
+    
+    if (doingGood == false) {
+        blinkColor();
+        roundSum.innerHTML = "Wrong";
+        setTimeout(() => {
+            roundSum.innerHTML = turn;
+            resetColor();
+            
+            if(strictMode) {
+                playGame();
+            } else {
+                gameTurn = true;
+                blink; 0;
+                playerAssortment = [];
+                doingGood = true;
+                playTime = setInterval(gamesTurn, 800);
+            }
+        }, 800);
+        sound = false;
+    }
+    
+    if (turn == playerAssortment.length && doingGood && !victory) {
+        turn++;
+        playerAssortment = [];
+        gameTurn = true;
+        blink = 0;
+        roundSum.innerHTML = turn;
+        playTime = setInterval(gamesTurn, 800);
+    }
+}
+
+function gameVictory(){
+    blinkColor();
+    roundSum.innerHTML = "Victory!";
+    powerOn = false;
+    victory = true;
+    setTimeout(() => {
+        resetColor();
+        playGame();
+    }, 1900);
+}
